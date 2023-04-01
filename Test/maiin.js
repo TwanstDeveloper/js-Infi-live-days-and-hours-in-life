85237e41ac60ede69e22637723ac16baca242847
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-04-01 15:40:04 +0300

    file coding

---
+++ Test/maiin.js
@@ -0,0 +1,13 @@
+let x=prompt("Enter Your Age")
+function days(age){
+    let r=age*365;
+    return r;
+}
+let n=days(x);
+document.write("Your Live n Eearth = "+n+" Days<br>");
+function hours(hs){
+    let g=hs*24;
+    return g;
+}
+let h=hours(n);
+document.write("Your Live n Eearth = "+h+" Hours");
